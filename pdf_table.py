77415ce23668ddf02d596513c298f05275ddde15
Juliana Correa <jucorrea.mail@gmail.com>
2012-11-28 20:06:18 -0200

    refactoring

--- pdf_table.py
+++ pdf_table.py
@@ -30,23 +30,28 @@ def get_page_elements(page):
 
 def get_page_text(page):
     page_elements = get_page_elements(page)
-    text_blocks = filter(lambda x: isinstance(x, LTTextBox) or isinstance(x, LTTextLine), page_elements)
+    istext = lambda el: isinstance(el, LTTextBox) or isinstance(el, LTTextLine)
+    text_blocks = filter(istext, page_elements)
     return [line for block in text_blocks for line in block]
 
+def update_line(lines, l, block):    
+    """
+        appends a text block to a line and updates the dict key so that it contains 
+        the average of y0 values for all its elements
+    """
+    i = len(lines[l])
+    lines[l].append(block)
+    new_key = (l * i + block.y0) / (i + 1)
+    lines[new_key] = lines.pop(l)
+    
 def compose_lines(text, avg_line_height):
     lines = {}
     for block in text:
-        found = False
         for l in lines:
             if abs(l - block.y0) < avg_line_height * 0.8:
-                found = True
-                lines[l].append(block)
-                #adjust dict key to the average y0
-                i = len(lines[l])
-                new_key = (l * (i-1) + block.y0) / i
-                lines[new_key] = lines.pop(l)
-                break    
-        if not found:
+                update_line(lines, l, block)
+                break
+        else:
             lines[block.y0] = [block]
     return lines
 
@@ -57,10 +62,6 @@ if __name__ == "__main__":
     pages = get_doc_pages('/home/ju/Downloads/A_B.pdf')
     page = pages.next() # for i, page in enumerate(pages):
     text = get_page_text(page)
-#   for block in text:
-#        if 435 <= block.x0 and block.x0  <= 448:
-#        if block.y0 > 240 and block.y0 < 242:
-#            print "[%s][w=%03f]: %s" % (block.x0, block.x1 - block.x0, block.get_text())
     lines = compose_lines(text, 10.5)
     for line_number in sorted(lines.keys(), reverse=True):
         print "[%s]" % line_number
